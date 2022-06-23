eb4f2d79b5e6bdaa290020b2cc0032ae1c5f1beb
GowthamsGit <105589846+GowthamsGit@users.noreply.github.com>
2022-06-24 01:47:59 +0530

    Update Sample.java

--- Sample.java
+++ Sample.java
@@ -6,6 +6,5 @@ System.out.println("Shanthakumari");
 System.out.println("Sheela");
 System.out.println("Priya");
 System.out.println("Divya"); // newly added
-System.out.println("Nisha"); // newly added
-System.out.println("Kavya"); // newly added 
+System.out.println("Nisha"); // newly added 
 }
